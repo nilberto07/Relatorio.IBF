47b79745fe6f2de9b0ebdc61fe0186e0f7b0a0a3
nilberto07 <nilberto07@gmail.com>
2026-06-05 00:28:26 -0300

    + deploy no Railway

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p .streamlit
+cat > .streamlit/secrets.toml << EOF
+[connections.gsheets]
+spreadsheet = "https://docs.google.com/spreadsheets/d/1xk0tzk-dN-sgqNoGK4ruHK9Uwc2emfwUFcfU1cNXs_o/edit?gid=0#gid=0"
+EOF
+streamlit run dashboard_financeiro.py --server.port $PORT --server.address 0.0.0.0
