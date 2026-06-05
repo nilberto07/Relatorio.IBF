4cb378f649bd6e419458731a7823aa91aab1e1db
nilberto07 <nilberto07@gmail.com>
2026-06-05 00:49:00 -0300

    + ajuste setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ cat > .streamlit/secrets.toml << EOF
 [connections.gsheets]
 spreadsheet = "https://docs.google.com/spreadsheets/d/1xk0tzk-dN-sgqNoGK4ruHK9Uwc2emfwUFcfU1cNXs_o/edit?gid=0#gid=0"
 EOF
-streamlit run dashboard_financeiro.py --server.port $PORT --server.address 0.0.0.0
+streamlit run app.py --server.port $PORT --server.address 0.0.0.0
